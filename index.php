766a410185a9eeb24a3bd742a449a9b027dd6808
jtmax1 <johntochukwumax@gmail.com>
2021-04-29 14:32:26 +0100

    initial

---
+++ index.php
@@ -0,0 +1,37 @@
+<!-- this is an implementation of HTML into PHP -->
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>A Simple PHP File</title>
+</head>
+<body>
+    <h1> 
+        
+
+        <?php
+        # This is a single line comment.
+        echo "Hello World! <br>";
+
+
+
+        // Declaring a variable.
+        $txt = "Hello World";
+        $number = 10;
+
+        //this define constant
+        define("SITE_URL", "www.jtmax.com/");
+
+            // Using the constant
+            echo "Thank You for visiting - " .SITE_URL . "<br>";
+
+            //Displaying variable values
+            echo "The text is - " .$txt . "<br>";
+            echo "The number is - " .$number . "<br>";
+        
+        ?>
+    
+    </h1>
+</body>
+</html>
